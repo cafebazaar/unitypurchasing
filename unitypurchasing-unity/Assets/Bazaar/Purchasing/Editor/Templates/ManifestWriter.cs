38c3fdd5dc7a67291677d5a28d1803e6ee7cf204
Mansour <mansurjavadi@gmail.com>
2022-05-19 23:51:34 +0430

    Move to Templates

--- unitypurchasing-unity/Assets/Bazaar/Purchasing/Editor/Templates/ManifestWriter.cs
+++ unitypurchasing-unity/Assets/Bazaar/Purchasing/Editor/Templates/ManifestWriter.cs
@@ -1,8 +1,8 @@
 #if UNITY_EDITOR
 using System.IO;
-using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 using UnityEngine.Purchasing;
 
 class ManifestWriter : IPreprocessBuildWithReport
@@ -12,21 +12,23 @@ class ManifestWriter : IPreprocessBuildWithReport
     public void OnPreprocessBuild(BuildReport report)
     {
         // Load AppStore
-        AppStore appStore = StoreData.LoadStore();
+        var appStore = StoreData.LoadStore();
 
         // Load Manifest Template
-        StoreData storeData = StoreData.data.ContainsKey(appStore) ? StoreData.data[appStore] : new StoreData();
-        string thisScriptDir = Path.GetDirectoryName(new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName());
+        var storeData = StoreData.data.ContainsKey(appStore) ? StoreData.data[appStore] : new StoreData();
+        var thisScriptDir = Path.GetDirectoryName(new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName());
         var manifestTempPath = Path.Combine(thisScriptDir, "AndroidManifestTemp.xml");
-        string manifest = File.ReadAllText(manifestTempPath);
+        var manifest = File.ReadAllText(manifestTempPath);
 
         // Replace Manifest 
         manifest = manifest.Replace("___MarketQueries___", storeData.manifestQueries);
         manifest = manifest.Replace("___MarketPermissions___", storeData.manifestPermission);
         manifest = manifest.Replace("___MarketActivities___", storeData.manifestActivity);
         manifest = manifest.Replace("___MarketReceivers___", storeData.manifestReceiver);
-        string androidManifestPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Assets", "Plugins", "Android", "AndroidManifest.xml");
+        var androidManifestPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Assets", "Plugins", "Android", "AndroidManifest.xml");
         File.WriteAllText(androidManifestPath, manifest);
+
+        }
     }
 }
 #endif
